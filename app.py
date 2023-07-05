930e3cf9118958a820e644c9ff2bee00cd2e18eb
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-07-05 23:35:49 +0700

    add post method with status 201

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-stores = {
+stores = [{
     "name": "My Store",
     "items": [
         {
@@ -10,8 +10,16 @@ stores = {
             "price": 15.99
         }
     ]
-}
+}]
 
 @app.get("/store")
 def get_store():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
