9e3c6c22e4964e557b531b33356d61770680f1a6
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-07-15 20:31:47 +0700

    feat: set up authentication with jwt

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.tag import blp as TagBlueprint
+load_dotenv()
 
 def create_app(db_url=None):
 
@@ -23,6 +26,9 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
