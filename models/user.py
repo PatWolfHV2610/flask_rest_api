92e22a3be9e9c687eed4360eeb7ca9aa2a4fa4a7
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-07-16 11:17:36 +0700

    feat: add user model

--- models/user.py
+++ models/user.py
@@ -5,6 +5,6 @@ class UserModel(db.Model):
 
     __tablename__ = "user"
 
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
