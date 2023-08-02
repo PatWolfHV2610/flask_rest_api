68ca49c48ccd66ab3d9ef71fbd1672cb02c981ba
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-08-02 20:18:45 +0700

    refactor: app.py

--- app.py
+++ app.py
@@ -14,12 +14,10 @@ from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
 from blocklist import BLOCKLIST
 
-load_dotenv()
-
 
 def create_app(db_url=None):
-
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Store REST API"
@@ -69,7 +67,7 @@ def create_app(db_url=None):
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
-        return jsonify({"message":"The token has expired", "error": "Token expired"}, 401)
+        return jsonify({"message": "The token has expired", "error": "Token expired"}, 401)
 
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
@@ -78,10 +76,10 @@ def create_app(db_url=None):
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return jsonify(
-                {"message": "Request dose not contain token",
-                 "error": "authorization_required"},
-                401
-            )
+            {"message": "Request dose not contain token",
+             "error": "authorization_required"},
+            401
+        )
 
     # with app.app_context():
     #     db.create_all()
@@ -93,5 +91,3 @@ def create_app(db_url=None):
     app.register_blueprint(UserBlueprint)
 
     return app
-
-
