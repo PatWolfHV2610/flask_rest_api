c84b8b54037b5c65ade2d347e9817a61ea2fe879
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-08-02 20:20:57 +0700

    fix: update foreign key on tag table point to store_id

--- models/tag.py
+++ models/tag.py
@@ -7,7 +7,7 @@ class TagModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
-    store_id = db.Column(db.String(), db.ForeignKey("store.id"), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("store.id"), nullable=False)
 
     store = db.relationship("StoreModel", back_populates="tags")
     items = db.relationship("ItemModel", back_populates="tags", secondary="item_tag")
