1e9a43e625c2dfef1407f1dedd571089a50bbc58
PatWolf2610 <nguyenhoangviethustk63@gmail.com>
2023-08-02 22:19:57 +0700

    feat: add email sending background job via redis

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+import redis
 from dotenv import load_dotenv
 
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from rq import Queue
 
 from db import db
 import models
@@ -18,6 +20,11 @@ from blocklist import BLOCKLIST
 def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
+
+    connection = redis.from_url(
+        os.getenv("REDIS_URI")
+    )
+    app.queue = Queue("emails", connection=connection)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Store REST API"
--- resources/user.py
+++ resources/user.py
@@ -1,16 +1,19 @@
+from flask import current_app
 from blocklist import BLOCKLIST
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
 from flask_jwt_extended import \
     (
-        create_access_token, jwt_required, get_jwt,
-        create_refresh_token, get_jwt_identity
-     )
+    create_access_token, jwt_required, get_jwt,
+    create_refresh_token, get_jwt_identity
+)
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
+from task import send_user_registration
 
 blp = Blueprint("users", "users", description="Operation with users")
 
@@ -18,16 +21,22 @@ blp = Blueprint("users", "users", description="Operation with users")
 @blp.route("/register")
 class UserRegister(MethodView):
 
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+        if UserModel.query.filter(
+                or_(
+                    UserModel.username == user_data["username"]),
+                UserModel.email == user_data['email']
+        ).first():
             abort(400, message="User with this name already exist")
         user = UserModel(username=user_data["username"],
-                         password=pbkdf2_sha256.hash(user_data["password"]))
+                         password=pbkdf2_sha256.hash(user_data["password"]),
+                         email=user_data['email'])
 
         db.session.add(user)
         db.session.commit()
+
+        current_app.queue.enqueue(send_user_registration, user.email, user.username)
         return {"message": "User create successfully"}, 201
 
 
